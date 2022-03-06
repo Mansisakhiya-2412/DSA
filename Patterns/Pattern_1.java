f72c1a842937f30662e6ac9c3c097d4ce49c7b1b
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-06 22:28:47 +0530

    pattern-1

---
+++ Patterns/Pattern_1.java
@@ -0,0 +1,19 @@
+ import java.util.Scanner;
+
+public class Pattern_1 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        for( int row = 1 ; row <= n  ; row++) {
+            int nst = row;
+
+            for( int i = 1 ; i <= nst ; i++) {
+                System.out.print("*\t");
+            }
+            System.out.println(" ");
+        }
+    }
+}
