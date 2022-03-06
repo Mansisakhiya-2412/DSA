06087cda231e273ce12e7626c5333b1df2bdf62d
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-07 00:07:57 +0530

    pattern-8 done

---
+++ Patterns/pattern_8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class pattern_8 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n = scn.nextInt();
+
+        for(int row = 1 ; row <= n ;row++ ){
+            for(int col = 1 ; col <= n ; col++){
+                if(row + col == n+1){
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
