e88c671fbd4c1c77c67ce680e698520ae1258d3a
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-08 12:31:28 +0530

    multiplication table

---
+++ Patterns/pattern_14.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class pattern_14 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        //int sum = 0;
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.println(n + " * "+i+" = "+n*i);
+        }
+
+    }
+}
