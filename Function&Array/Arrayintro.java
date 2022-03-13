6018d961660886fb46a5763f255d0dd79e42f623
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-03-14 01:21:45 +0530

    intro to arrays

---
+++ Function&Array/Arrayintro.java
@@ -0,0 +1,37 @@
+public class Arrayintro {
+
+    public static void main(String arg[]) {
+
+        int arr[] = new int[5];
+
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[4]);
+
+        arr[0] = 5;
+        arr[1] = 1;
+        arr[2] = -3;
+        arr[3] = 2;
+
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[4]);
+
+        // System.out.println("length : "+arr.length);
+
+        // for( int idx = 0 ; idx <= arr.length ; idx++){
+        //     int val = arr[idx];
+        //     System.out.print(val+" ");
+        // }
+        // System.out.println();
+
+        for(int pepcoding : arr){
+            System.out.print(pepcoding+" ");
+        }
+        System.out.println();
+    }
+}
