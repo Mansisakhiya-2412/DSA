ef7c57a1e18bf0e7146e0d8f92ee03bbfb5a43e1
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-04-07 21:55:00 +0530

    broken economy

---
+++ Function&Array/BrokenEconomy.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class BrokenEconomy{
+
+public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ; i < arr.length ; i++){
+        arr[i] = scn.nextInt();
+    }
+
+    int k = scn.nextInt();
+    
+    binarySearch(arr,k);
+ }
+
+ public static void binarySearch(int[] arr , int k) {
+     int lo = 0 , hi = arr.length-1 ;
+
+     int ceil =0, floor = 0;
+
+     while(lo<=hi){
+         int mid = (lo + hi) / 2 ;
+
+         if(k < arr[mid])
+         {
+             hi = mid -1;
+             ceil = arr[mid];
+         }
+         else if(k > arr[mid])
+         {
+             lo = mid +1;
+             floor = arr[mid];
+         }
+         else{
+             ceil = arr[mid];
+             floor = arr[mid];
+         }
+     }
+     System.out.println(ceil);
+     System.out.println(floor);
+ }
+
+}
