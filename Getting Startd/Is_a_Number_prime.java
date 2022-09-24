f2ab0d355b29539650bed8006cf08eb61df50a66
Mansisakhiya-2412 <mansisakhiya9899@gmail.com>
2022-09-24 10:33:19 +0530

    link

--- Getting Startd/Is_a_Number_prime.java
+++ Getting Startd/Is_a_Number_prime.java
@@ -71,3 +71,5 @@ public class Is_a_Number_prime {
 
     }
 }
+
+https://www.figma.com/proto/Fd0iI85Cb8j9lyMee11ha3/Dreamhouse?node-id=111%3A21&scaling=scale-down-width&page-id=0%3A1&starting-point-node-id=1%3A2
